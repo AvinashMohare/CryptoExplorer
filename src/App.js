9583ac32f2c310e59f461ed5cf51693966b4423b
avinashmohare <mohareavinash80@gmail.com>
2024-01-31 00:02:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from "antd";
+import Navbar from "./components/navbar";
+import Exchanges from "./components/exchanges";
+import Homepage from "./components/homepage";
+
+import Cryptocurrencies from "./components/cryptocurrencies";
+import CryptoDetails from "./components/cryptoDetails";
+import News from "./components/news";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app">
+            <div className="navbar">
+                <Navbar />
+            </div>
+
+            <div className="main">
+                <Layout>
+                    <div className="routes">
+                        <Routes>
+                            <Route path="/" element={<Homepage />} />
+
+                            <Route path="/exchanges" element={<Exchanges />} />
+
+                            <Route
+                                path="/cryptocurrencies"
+                                element={<Cryptocurrencies />}
+                            />
+
+                            <Route
+                                path="/crypto/:coinId"
+                                element={<CryptoDetails />}
+                            />
+
+                            <Route path="/news" element={<News />} />
+                        </Routes>
+                    </div>
+                </Layout>
+
+                <div className="footer">
+                    <Typography.Title
+                        level={5}
+                        style={{ color: "white", textAlign: "center" }}
+                    >
+                        Cryptoverse <br></br>
+                        All rights reserved
+                    </Typography.Title>
+
+                    <Space>
+                        <Link to="/">
+                            <Typography.Text style={{ color: "white" }}>
+                                Home
+                            </Typography.Text>
+                        </Link>
+                        <Link to="/exchanges">
+                            <Typography.Text style={{ color: "white" }}>
+                                Exchanges
+                            </Typography.Text>
+                        </Link>
+                        <Link to="/news">
+                            <Typography.Text style={{ color: "white" }}>
+                                News
+                            </Typography.Text>
+                        </Link>
+                    </Space>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
